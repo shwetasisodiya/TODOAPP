6fd770fa85ec40ad1e1d1a1c1d756c59b9a789f4
shwetasisodiya <sisodiyashweta5@gmail.com>
2024-04-04 11:15:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import {useState} from 'react';
+import { Form } from "./components/Form";
+import { Todos } from "./components/Todos";
+import {useDispatch, useSelector} from 'react-redux';
+import {deleteAll} from './redux/todoapp/actions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+  // dispatch function to dispatch an action
+  const dispatch = useDispatch();
+
+  // getting todos state for conditional rendering
+  const todos = useSelector((state)=>state.operationsReducer);
+
+  // update form visibility state
+  const [editFormVisibility, setEditFormVisibility]=useState(false);
+
+  // editTodo state
+  const [editTodo, setEditTodo]=useState('');
+
+  // this function will trigger when someone clicks the edit icon
+  const handleEditClick=(todo)=>{
+    setEditFormVisibility(true);
+    setEditTodo(todo);
+  }
+
+  // back button click
+  const cancelUpdate=()=>{
+    setEditFormVisibility(false);
+  }
+
+    return (
+    <div className="wrapper">
+      <br></br>
+      <h1 className="text-center">TODO-APP USING REACT-REDUX</h1>
+      <Form editFormVisibility={editFormVisibility} editTodo={editTodo}
+      cancelUpdate={cancelUpdate}/>
+      <Todos handleEditClick={handleEditClick} editFormVisibility={editFormVisibility}/>
+      {todos.length > 1 && (
+        <button className='btn btn-danger btn-md delete-all'
+        onClick={()=>dispatch(deleteAll())}>DELETE ALL</button>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import {store} from './redux/store';
+import {Provider} from 'react-redux';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
